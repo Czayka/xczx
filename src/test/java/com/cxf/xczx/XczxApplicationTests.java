3e1398716c99882c86aaccc2e75a8ea84e6ae403
Administrator <1606007912@qq.com>
2023-04-26 20:02:19 +0800

    create

---
+++ src/test/java/com/cxf/xczx/XczxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxf.xczx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XczxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
